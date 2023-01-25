c538951dbd82fe3a6c5c7063e773fa0bc56c4de7
root <root@k8s-2-int.adm13>
2023-01-25 18:00:19 +0000

    .

---
+++ ansible/scripts/delete_image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+image="$1"
+manifest="$2"
+
+. set_env.sh
+
+set -x
+
+echo "deleting manifest, image: ${image}, manifest: ${manifest}"
+curl -v -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X DELETE "https://${registry}/v2/$image/manifests/${manifest}"
+
---
+++ ansible/scripts/list_images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. set_env.sh
+
+curl -s  -X GET https://${registry}/v2/_catalog | jq -r '.repositories[]' | while read img; do 
+  tags=$(curl -s -X GET https://${registry}/v2/$img/tags/list  | jq -r '.tags[]' 2>/dev/null); 
+  for t in $tags; do 
+    echo -n "img: $img tag: $t";
+    digest=$(curl -v -H  "Accept: application/vnd.docker.distribution.manifest.v2+json" -H "Accept: application/vnd.oci.image.manifest.v1+json" -X HEAD  https://${registry}/v2/$img/manifests/$t 2>&1 | grep docker-content-digest: | awk '{print $3}' | tr -d "\r");
+    echo " $digest" ;
+  done;
+done
