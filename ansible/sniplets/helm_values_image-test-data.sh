bfec560eb25dc5aafd1f0cdd513c8b450348adf4
root <root@k8s-2-int.adm13>
2023-05-20 21:27:32 +0000

    .

--- ansible/sniplets/helm_values_image-test-data.sh
+++ ansible/sniplets/helm_values_image-test-data.sh
@@ -3,40 +3,38 @@
 input="$1"
 
     yq -r 'path(..|.image? // empty) | [.[]|tostring]|join(".")' "${input}"  | while read image_section_org;do
-      echo
+          echo
       image_section=$(echo "${image_section_org}" | sed -e 's/^/"/' -e 's/$/"/' -e 's/\./"\."/g')
-      section_type=$(yq -r '.'"${image_section}"' | type' "${input}"  )
-      set -x
+      section_type=$(yq -r '.'"${image_section}"' | type' "${input}" )
       # namings pulling an image: <path>/<image>:<tag>
       if [ "${section_type}" == "string" ]; then # if image is only a string use the value as repository
-        parent=$(yq -r 'path(.'"${image_section}"') | .[:-1] |join(".")' "${input}" )
-        parent_tag=$(yq -r '.'"${parent}"'.tag? // empty ' "${input}"  )
+        parent=$(yq -r 'path(.'"${image_section}"') | .[:-1] |join(".")' "${input}")
+        parent_tag=$(yq -r '.'"${parent}"'.tag? // empty ' "${input}" )
 
-        image=$(yq -r '.'"${image_section}" "${input}"  )
-        image_path=$(echo "${image}" | grep -q : && echo "${image%/*}")
+        image=$(yq -r '.'"${image_section}" "${input}" )
+        image_path=$(echo "${image}" | grep -q / && echo "${image%/*}")
         image_itself=${image##*/}
-        [[ ${image_itself} == *:* && ${parent_tag} == "" ]] || image_itself="${image_itself}:${parent_tag}"
+        ## [[ ${image_itself} == *:* && ${parent_tag} == "" ]] || image_itself="${image_itself}:${parent_tag}"
 
-        repo=$(yq -r '.'"${parent}"'.repository? // empty ' "${input}"  )
-        [ -z "${repo}" ] && repo=$(yq -r '.'"${parent}"'.hub? // empty ' "${input}"  ) # prefer parent.repository over parent.hub
+        repo=$(yq -r '.'"${parent}"'.repository? // empty ' "${input}" )
+        [ -z "${repo}" ] && repo=$(yq -r '.'"${parent}"'.hub? // empty ' "${input}" ) # prefer parent.repository over parent.hub
       else
-        parent_tag=$(yq -r '.'"${image_section}"'.tag? // empty' "${input}" )
+        parent_tag=$(yq -r '.'"${image_section}"'.tag? // empty' "${input}")
 
-        image=$(yq -r '.'"${image_section}"'.repository? // empty' "${input}" )
-        image_path=$(echo "${image}" | grep -q : && echo "${image%/*}")
+        image=$(yq -r '.'"${image_section}"'.repository? // empty' "${input}")
+        [[ ${image} == "" ]] && image=$(yq -r '.'"${image_section}"'.repo? // empty' "${input}")
+        image_path=$(echo "${image}" | grep -q / && echo "${image%/*}")
         image_itself=${image##*/}
-        [[ ${image_itself} == *:* && ${parent_tag} == "" ]] || image_itself="${image_itself}:${parent_tag}"
+        ## [[ ${image_itself} == *:* && ${parent_tag} == "" ]] || image_itself="${image_itself}:${parent_tag}"
 
         repo=
       fi
-      if [[ ${image_itself} != *:* ]]; then
-        # echo "tag is empty (${tag}), trying appVersion"
-        # tag=$(yq -r '.appVersion' Chart.yaml)
-        echo "tag is empty, trying latest"
-        #tag="latest" #$(yq -r '.appVersion' Chart.yaml)
-        image_itself="${image_itself}:latest"
-      fi
+      [[ ${image_itself} == "" ]] && continue
       fetch_img="${repo}/${image_path}/${image_itself}"
-      tag="${image_itself#*:}"
-      echo "treat image repository: ${repo} image: ${image_itself} fetch_img=${fetch_img} image_section=${image_section} section_type=${section_type} (from org:${image_section_org})"
-    done
+
+      [[ ${fetch_img} != *:* && ${parent_tag} != "" ]] && fetch_img="${fetch_img}:${parent_tag}"
+      [[ ${fetch_img} == *:* ]] && tag="${fetch_img#*:}"
+      fetch_img=$(echo "$fetch_img" | sed -e 's#/\+#/#' -e 's#^/\+##')
+      echo "treat image repository: ${repo} image: ${image_itself} fetch_img: ${fetch_img} image_section=${image_section} section_type=${section_type} (from org:${image_section_org})"
+
+done
