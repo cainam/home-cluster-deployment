cf36ac4e64f2b24b46c28c1d39d68d97be2156a1
root <root@k8s-1-int.adm13>
2024-01-07 19:43:33 +0000

    .

--- ansible/scripts/keepalive.sh
+++ ansible/scripts/keepalive.sh
@@ -3,7 +3,7 @@
 date >> /tmp/ll
 echo "$0 $1 $2 $3" >> /tmp/ll
 
-if [ "$3" = "MASTER" ]; then
-  /etc/init.d/run-registry restart
-  /etc/init.d/lighttpd restart 
-fi
+#if [ "$3" = "MASTER" ]; then
+#  /etc/init.d/run-registry restart
+#  /etc/init.d/lighttpd restart 
+#fi
