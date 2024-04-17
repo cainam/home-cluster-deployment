9b008005922c5744ed603d2b25d5f0e32992f01c
root <root@k8s-3-int.adm13>
2024-04-17 16:11:02 +0000

    .

--- ansible/scripts/deploy_gentoo.sh
+++ ansible/scripts/deploy_gentoo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # random delay
-sleep $(($RANDOM/100))
+#sleep $(($RANDOM/100))
 
 my_name=$(basename $0)
 run_file="/var/run/${my_name}"
@@ -9,12 +9,19 @@ run_file="/var/run/${my_name}"
 . /usr/local/bin/set_env.sh 
 cd /data/mine/git/ansible/
 
-[ -f "${run_file}" ] && exit 0
-touch "${run_file}"
+#[ -f "${run_file}" ] && exit 0
+#touch "${run_file}"
+procs=$(pgrep -c -f "${my_name}")
+if [ $procs -gt 1 ]; then
+  message="already $procs processes running of ${my_name} => exit"
+  logger "${message}"
+  echo "${message}"
+  exit
+fi
 
 logger "starting $my_name"
 
 eval $my_ansible /data/mine/git/ansible/site.yml --tags=gentoo,emerge
 
-rm "${run_file}"
+#rm "${run_file}"
 logger "finished $my_name"
