f430409c0ba8c7898d2836dbd24440e007e7c8c9
root <root@k8s-2-int.adm13>
2024-06-05 18:16:05 +0000

    .

--- ansible/scripts/set_env.sh
+++ ansible/scripts/set_env.sh
@@ -6,15 +6,8 @@ export category=$1
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-#export images_list="/data/meinKram/conf/images.list"
-#export images_map="/data/meinKram/conf/images.map"
-
-#export registry="myregistry.adm13:443"
-
 export helm_dir=/shared/helm
 export helm_options="--repository-config $helm_dir/config --repository-cache $helm_dir/cache "
-#export xpodman_options="-t --rm --network host -v /tmp:/tmp -v /etc/kubernetes:/etc/kubernetes -v /etc/ssl/certs:/etc/ssl/certs -v $helm_dir:$helm_dir --workdir $PWD -v $PWD:$PWD -e KUBECONFIG=$KUBECONFIG"
-#alias helmx="podman run ${podman_options} myregistry.adm13:443/helm:v3.10.2 $helm_options"
 function helm(){
   export podman_options="-t --rm --network host -v /tmp:/tmp -v /etc/kubernetes:/etc/kubernetes -v /etc/ssl/certs:/etc/ssl/certs -v $helm_dir:$helm_dir --workdir $PWD -v $PWD:$PWD -e KUBECONFIG=$KUBECONFIG"
   echo "podman_options:${podman_options}"
