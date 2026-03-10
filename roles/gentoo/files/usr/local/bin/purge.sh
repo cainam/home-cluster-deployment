cc0b9b202c1b48d09219e86f8def2b15d3f8a24b
root <root@k8s-2-int.adm13>
2026-03-10 22:21:31 +0100

    .

--- roles/gentoo/files/usr/local/bin/purge.sh
+++ roles/gentoo/files/usr/local/bin/purge.sh
@@ -30,7 +30,7 @@ podman image ls --format '{{.Repository}}:{{.Tag}} {{.ID}}' | sed -e "s#^${regis
   #id=$(echo "${image_with_id}" | cut -d : -f 3)
   echo "${image}" | grep -Eq "${protected_images}" && continue
   # this sadly deletes images in the making by podman build: echo "${image}" | grep -Eq "<none>:<none>$" && (echo "delete $image"; podman image rm "${id}") && continue
-  echo "${configured_images}" | grep -Eq "^${image}$|^${registry}/${image}$" || (echo "delete $image") #; podman image rm -f "$image")
+  echo "${configured_images}" | grep -Eq "^${image}$|^${registry}/${image}$" || (echo "delete $image"; podman image rm "$image")
 done
 
 # registry garbage collect
