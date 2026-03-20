83f7f7d8f55f643720c24bcc80a019b0b64044f9
root <root@k8s-2-int.adm13>
2026-03-20 07:55:18 +0100

    .

--- roles/gentoo/files/usr/local/bin/set_env.sh
+++ roles/gentoo/files/usr/local/bin/set_env.sh
@@ -2,6 +2,9 @@
 
 . /etc/conf.d/home-cluster
 
+source ${local_pyenv}/bin/activate
+
 export ANSIBLE_ROLES_PATH=${ANSIBLE_DIR}/roles
 export my_ansible="ANSIBLE_PIPELINING=True ANSIBLE_NO_TARGET_SYSLOG=True ANSIBLE_CALLBACK_RESULT_FORMAT=yaml ansible-playbook --extra-vars local_only=${LOCAL_CONF} -i inventory"
 export env_is_set=1
+
