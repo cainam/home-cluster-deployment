47fab610ae781735ab9f901a2a7d96eb5c126ed5
root <root@k8s-2-int.adm13>
2026-04-13 22:23:55 +0200

    .

--- application_playbooks/curator/files/curator.py
+++ application_playbooks/curator/files/curator.py
@@ -12,10 +12,14 @@ def configure(settings: kopf.OperatorSettings, **_):
     # Disable cluster-wide namespace scanning
     settings.scanning.disabled = True
     import logging
-    logging.getLogger().handlers.clear()  # optional but often helpful
 
-    kopf_logger = logging.getLogger('kopf')
-    kopf_logger.propagate = False
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    )
+
+    # Prevent duplication
+    logging.getLogger('kopf').propagate = False
 
 @kopf.on.field('v1', 'pods', field='status.containerStatuses')
 def handle_pull_failures(logger, old, new, name, namespace, spec, **kwargs):
